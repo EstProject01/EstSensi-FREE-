76052c974bfa832b368c1ecb9f3d139a5345bea5
EstProject01 <161734425+EstProject01@users.noreply.github.com>
2024-03-11 22:49:30 +0700

    Update set.sh

--- set.sh
+++ set.sh
@@ -3,7 +3,7 @@
 echo "==============================="
 echo " Welcome to EstSensi Installation "
 echo " Creator: @EstProject01 "
-echo " Version: 3.0 "
+echo " Version: 3.0 stable  "
 echo " Credit: EstProject "
 echo "==============================="
 
@@ -19,6 +19,8 @@ sleep 3
     echo " Launching EstSensi..."
     sleep 2
     echo ""
-    wm density 350
+    wm size 2000x4444
+    wm density 700
+    echo ""
     sleep 0.5
     am start -n com.dts.freefireth/com.dts.freefireth.FFMainActivity
