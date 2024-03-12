9fde177637341646108bac975a023cb756a39c6f
EstProject01 <161734425+EstProject01@users.noreply.github.com>
2024-03-13 03:58:56 +0700

    Add files via upload

---
+++ unset.sh
@@ -0,0 +1,34 @@
+#!/system/bin/bash
+
+# Define expiration date (format: YYYYMMDD)
+expiration_date=20240315
+
+# Check expiration date
+current_date=$(date +%Y%m%d)
+if [ $current_date -gt $expiration_date ]; then
+    echo " Error: Uninstallation is not allowed as the product has expired. Please contact EstProject for assistance."
+    exit 1
+fi
+
+echo "==============================="
+echo " Welcome to EstSensi Uninstaller "
+echo " Creator: @EstProject01 "
+echo " Version: 3.0 stable "
+echo " Credit: EstProject "
+echo " Build Date: 09-03-2024 "
+echo "==============================="
+
+echo " Wait for EstSensi uninstallation [ 5 seconds ] "
+echo ""
+sleep 5
+
+wm size reset
+sleep 1
+echo " reset size success "
+echo ""
+wm density reset
+sleep 1
+echo " density reset success "
+echo ""
+sleep 2
+echo " Uninstallation Successfully "
